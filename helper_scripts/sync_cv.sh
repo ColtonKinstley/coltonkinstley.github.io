ce7487ac429088abc056dafbebcc7dced61df1c9
Colton <colton.kinstley@gmail.com>
2018-09-09 17:02:46 +0100

    Updated CV.

---
+++ helper_scripts/sync_cv.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+CV="$HOME/Dropbox/CV - Admissions/cv_latex/Awesome-CV/MyCV/cv.pdf"
+WEBSITE_CV="$HOME/Dev/website/coltonkinstley.github.io/assets/ckinstley_cv.pdf"
+cp -v "$CV" "$WEBSITE_CV"
